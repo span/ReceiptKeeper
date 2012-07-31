70d5c17b65c97092b770298db5a87bf7242d0b19
Daniel Kvist <daniel@danielkvist.net>
2012-07-31 14:38:03 +0200

    Fixed add activity scrolling and map centering

--- src/net/danielkvist/receipttracker/activity/MyMapActivity.java
+++ src/net/danielkvist/receipttracker/activity/MyMapActivity.java
@@ -3,42 +3,59 @@ package net.danielkvist.receipttracker.activity;
 import net.danielkvist.receipttracker.R;
 import android.os.Bundle;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 
-public class MyMapActivity extends MapActivity {
-    
+public class MyMapActivity extends MapActivity
+{
+
     private MapView mapView;
     private MyLocationOverlay mlo;
 
     @Override
-    protected void onCreate(Bundle icicle) {
+    protected void onCreate(Bundle icicle)
+    {
         super.onCreate(icicle);
         setContentView(R.layout.my_map_activity);
-        
+
         mapView = (MapView) findViewById(R.id.mapview);
-        
+        mapView.setBuiltInZoomControls(true);
+
+        MapController mapController = mapView.getController();
+        mapController.setZoom(10);
+
         mlo = new MyLocationOverlay(this, mapView);
         mlo.enableMyLocation();
-        
+
         mapView.getOverlays().add(mlo);
         mapView.postInvalidate();
     }
-    
+
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         mlo.enableMyLocation();
+        mlo.runOnFirstFix(new Runnable()
+        {
+            public void run()
+            {
+                mapView.getController().animateTo(mlo.getMyLocation());
+            }
+        });
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause()
+    {
         super.onPause();
         mlo.disableMyLocation();
     }
-    
+
     @Override
-    protected boolean isRouteDisplayed() {
+    protected boolean isRouteDisplayed()
+    {
         return false;
     }
 }
