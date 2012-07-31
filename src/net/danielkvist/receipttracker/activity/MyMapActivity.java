52294f720de47e2b55c3ec4def7ad57eba58d644
Daniel Kvist <daniel@danielkvist.net>
2012-07-31 13:55:23 +0200

    Fixed MapView  and current location

--- src/net/danielkvist/receipttracker/activity/MyMapActivity.java
+++ src/net/danielkvist/receipttracker/activity/MyMapActivity.java
@@ -3,13 +3,38 @@ package net.danielkvist.receipttracker.activity;
 import net.danielkvist.receipttracker.R;
 import android.os.Bundle;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
 
 public class MyMapActivity extends MapActivity {
     
+    private MapView mapView;
+    private MyLocationOverlay mlo;
+
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.my_map_activity);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+        
+        mlo = new MyLocationOverlay(this, mapView);
+        mlo.enableMyLocation();
+        
+        mapView.getOverlays().add(mlo);
+        mapView.postInvalidate();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mlo.enableMyLocation();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mlo.disableMyLocation();
     }
     
     @Override
