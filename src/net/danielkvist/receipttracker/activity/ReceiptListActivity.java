4a0564c2a468c0f7866c84210906a2b5c3662abc
Daniel Kvist <daniel@danielkvist.net>
2012-08-01 12:45:43 +0200

    Show newly added receipt information on start activity

--- src/net/danielkvist/receipttracker/activity/ReceiptListActivity.java
+++ src/net/danielkvist/receipttracker/activity/ReceiptListActivity.java
@@ -1,11 +1,17 @@
 package net.danielkvist.receipttracker.activity;
 
+import org.w3c.dom.Text;
+
 import net.danielkvist.receipttracker.R;
+import net.danielkvist.receipttracker.content.Receipt;
 import net.danielkvist.receipttracker.fragment.ReceiptDetailFragment;
 import net.danielkvist.receipttracker.fragment.ReceiptListFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ReceiptListActivity extends FragmentActivity implements ReceiptListFragment.Callbacks
 {
@@ -16,6 +22,7 @@ public class ReceiptListActivity extends FragmentActivity implements ReceiptList
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        // TODO Check if possible to change layout file to another, refactor the current file
         setContentView(R.layout.activity_receipt_list);
 
         if (findViewById(R.id.receipt_detail_container) != null)
@@ -24,6 +31,34 @@ public class ReceiptListActivity extends FragmentActivity implements ReceiptList
             ((ReceiptListFragment) getSupportFragmentManager().findFragmentById(R.id.receipt_list))
                     .setActivateOnItemClick(true);
         }
+        
+        Intent intent = getIntent();
+        final Receipt receipt = (Receipt) intent.getParcelableExtra(Receipt.EXTRA_RECEIPT);
+        if(receipt != null)
+        {
+            LinearLayout container = ((LinearLayout) findViewById(R.id.receipt_added_container));
+            container.setVisibility(View.VISIBLE);
+            container.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    showDetail(receipt);
+                }
+            });
+            TextView receiptName = (TextView) findViewById(R.id.receipt_name);
+            TextView receiptSum = (TextView) findViewById(R.id.receipt_sum);
+            TextView receiptDateAndTime = (TextView) findViewById(R.id.receipt_date_and_time);
+            
+            receiptName.setText(getString(R.string.name) + ": " + receipt.getName() + ", ");
+            receiptSum.setText(getString(R.string.cost) + ": " + receipt.getSum());
+            receiptDateAndTime.setText(getString(R.string.date) + ": " + receipt.getDate() + " - " + receipt.getTime());
+        }
+    }
+
+    protected void showDetail(Receipt receipt)
+    {
+        // TODO Launch the detail activity with the receipt
     }
 
     @Override
