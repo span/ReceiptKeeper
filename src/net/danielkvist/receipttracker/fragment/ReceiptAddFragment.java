27390c79f9fa424ff29e2db510cf57005ff7cc6d
Daniel Kvist <daniel@danielkvist.net>
2012-08-16 02:40:16 +0200

    Now adding and updating accounts as needed

--- src/net/danielkvist/receipttracker/fragment/ReceiptAddFragment.java
+++ src/net/danielkvist/receipttracker/fragment/ReceiptAddFragment.java
@@ -14,6 +14,7 @@ import net.danielkvist.receipttracker.activity.MyMapActivity;
 import net.danielkvist.receipttracker.activity.MyMapFragmentActivity;
 import net.danielkvist.receipttracker.content.Receipt;
 import net.danielkvist.receipttracker.content.ReceiptAccount;
+import net.danielkvist.receipttracker.fragment.AddReceiptAccountDialog.AddReceiptAccountDialogListener;
 import net.danielkvist.util.Communicator;
 import net.danielkvist.util.Setting;
 import net.danielkvist.util.task.ScaleBitmapFileTask;
@@ -28,6 +29,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -45,9 +47,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ReceiptAddFragment extends Fragment implements OnDateSetListener, DialogInterface.OnClickListener
+public class ReceiptAddFragment extends Fragment implements OnDateSetListener, DialogInterface.OnClickListener,
+        AddReceiptAccountDialogListener
 {
-    
+
     public static final int MEDIA_TYPE_IMAGE = 1;
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private String filename;
@@ -63,6 +66,7 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
     private DatePickerFragment datePickerFragment;
     private Spinner accountSpinner;
     private ArrayList<ReceiptAccount> receiptAccounts;
+    private ImageView accountAddButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -72,45 +76,44 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
         setHasOptionsMenu(true);
         setRetainInstance(true);
         applicationContext = getActivity().getApplicationContext();
-        if(savedInstanceState != null)
+        if (savedInstanceState != null)
             receipt = (Receipt) savedInstanceState.getParcelable(Receipt.EXTRA_RECEIPT);
         else
             receipt = (Receipt) getArguments().getParcelable(Receipt.EXTRA_RECEIPT);
     }
-    
+
     @Override
-    public void onPrepareOptionsMenu(Menu menu) 
+    public void onPrepareOptionsMenu(Menu menu)
     {
         MenuItem item = menu.findItem(R.id.item_save);
         item.setVisible(true);
-        
+
         super.onPrepareOptionsMenu(menu);
     }
-    
+
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        switch(item.getItemId()) 
+        switch (item.getItemId())
         {
             case R.id.item_save:
                 saveReceipt();
                 return true;
         }
-        
+
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        
-        if(receipt == null)
+
+        if (receipt == null)
         {
             receipt = new Receipt();
         }
-        
+
         HashMap<String, Integer> settingsMap = communicator.getAllSettings();
-        
-        
+
         View rootView = inflater.inflate(R.layout.fragment_receipt_add, container, false);
         imageView = (ImageView) rootView.findViewById(R.id.receipt_photo_image_view);
         imageView.setOnClickListener(new View.OnClickListener()
@@ -119,7 +122,7 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
             public void onClick(View v)
             {
                 ImageView i = (ImageView) v;
-                if(i.getDrawable() == null)
+                if (i.getDrawable() == null)
                 {
                     takePhoto();
                 }
@@ -130,96 +133,132 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
                     intent.setDataAndType(Uri.parse("file:///" + receipt.getPhoto()), "image/*");
                     startActivity(intent);
                 }
-                
+
             }
         });
-        
-        
+
         showBitmap();
-        
+
         nameView = (EditText) rootView.findViewById(R.id.add_receipt_name);
         nameView.setText(receipt.getName());
-        
+
         sumView = (EditText) rootView.findViewById(R.id.add_receipt_sum);
         sumView.setVisibility(settingsMap.get(Setting.SETTING_FIELD_SUM));
         sumView.setText(receipt.getSum());
-        
+
         taxView = (EditText) rootView.findViewById(R.id.add_receipt_tax);
         taxView.setVisibility(settingsMap.get(Setting.SETTING_FIELD_TAX));
         taxView.setText(receipt.getTax());
-        
-        // FIXME Add possibility to add new account (launch dialog with id and name)
+
+        accountAddButton = (ImageView) rootView.findViewById(R.id.add_receipt_account_button);
+        accountAddButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                showEditDialog();
+
+            }
+        });
         accountSpinner = (Spinner) rootView.findViewById(R.id.add_receipt_account);
         accountSpinner.setVisibility(settingsMap.get(Setting.SETTING_FIELD_ACCOUNT));
         rootView.findViewById(R.id.add_receipt_account_label).setVisibility(settingsMap.get(Setting.SETTING_FIELD_ACCOUNT));
-        if(settingsMap.get(Setting.SETTING_FIELD_ACCOUNT) == View.VISIBLE)
+        if (settingsMap.get(Setting.SETTING_FIELD_ACCOUNT) == View.VISIBLE)
         {
             receiptAccounts = communicator.getReceiptAccounts();
-            List<String> list = new ArrayList<String>();
-            for(ReceiptAccount r : receiptAccounts)
-            {
-                StringBuilder sb = new StringBuilder();
-                sb.append(r.getCode());
-                sb.append(" - ");
-                sb.append(getResources().getString(getResources().getIdentifier(r.getName(), "string", "net.danielkvist.receipttracker")));
-                list.add(sb.toString());
-            }
-            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, list);
-            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            accountSpinner.setAdapter(dataAdapter);
-            if(receipt.getReceiptAccountId() > -1)
-            {
-                int position = findReceiptPosition(receipt.getReceiptAccountId());
-                accountSpinner.setSelection(position);
-            }
+            setReceiptAccountAdapter();
         }
 
         commentView = (EditText) rootView.findViewById(R.id.add_receipt_comment);
         commentView.setVisibility(settingsMap.get(Setting.SETTING_FIELD_COMMENT));
         commentView.setText(receipt.getComment());
-        
+
         timeView = (TextView) rootView.findViewById(R.id.add_receipt_timestamp);
         timeView.setText(receipt.getDateAndTime(applicationContext));
 
         ImageButton cameraButton = (ImageButton) rootView.findViewById(R.id.camera_button);
-        cameraButton.setOnClickListener(new View.OnClickListener() {
+        cameraButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) { takePhoto(); }
+            public void onClick(View v)
+            {
+                takePhoto();
+            }
         });
-        
+
         ImageButton timeButton = (ImageButton) rootView.findViewById(R.id.timestamp_button);
-        timeButton.setOnClickListener(new View.OnClickListener() {
+        timeButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) { showDateDialog(); }
+            public void onClick(View v)
+            {
+                showDateDialog();
+            }
         });
-        
+
         FrameLayout f = (FrameLayout) rootView.findViewById(R.id.map_container);
         f.setVisibility(settingsMap.get(Setting.SETTING_FIELD_LOCATION));
         rootView.findViewById(R.id.map_container_label).setVisibility(settingsMap.get(Setting.SETTING_FIELD_LOCATION));
-        
-        if(settingsMap.get(Setting.SETTING_FIELD_LOCATION) == View.VISIBLE)
+
+        if (settingsMap.get(Setting.SETTING_FIELD_LOCATION) == View.VISIBLE)
         {
             Bundle arguments = new Bundle();
             arguments.putParcelable(Receipt.EXTRA_RECEIPT, receipt);
-            
+
             Fragment fr = new MyMapFragmentActivity();
             fr.setArguments(arguments);
-            
+
             FragmentTransaction ft = getFragmentManager().beginTransaction();
             ft.add(R.id.map_container, fr);
             ft.commit();
         }
-        
-        
+
         return rootView;
     }
-    
+
+    private void showEditDialog()
+    {
+        FragmentManager fm = getFragmentManager();
+        AddReceiptAccountDialog dialog = new AddReceiptAccountDialog();
+        dialog.setCallback(this);
+        dialog.show(fm, "fragment_add_receipt_account");
+    }
+
+    private void setReceiptAccountAdapter()
+    {
+        List<String> list = new ArrayList<String>();
+        for (ReceiptAccount r : receiptAccounts)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(r.getCode());
+            sb.append(" - ");
+            if(r.isUserAdded())
+            {
+                sb.append(r.getName());
+            }
+            else
+            {
+                sb.append(getResources().getString(getResources().getIdentifier(r.getName(), "string", "net.danielkvist.receipttracker")));
+            }
+            
+            list.add(sb.toString());
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountSpinner.setAdapter(dataAdapter);
+        if (receipt.getReceiptAccountId() > -1)
+        {
+            int position = findReceiptPosition(receipt.getReceiptAccountId());
+            accountSpinner.setSelection(position);
+        }
+    }
+
     private int findReceiptPosition(long receiptAccountId)
     {
         int i = 0;
-        while(i < receiptAccounts.size())
+        while (i < receiptAccounts.size())
         {
-            if(receiptAccountId == receiptAccounts.get(i).getCode())
+            if (receiptAccountId == receiptAccounts.get(i).getCode())
                 break;
             i++;
         }
@@ -229,14 +268,14 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
     private void showDateDialog()
     {
         datePickerFragment = DatePickerFragment.newInstance();
-        
+
         // WORKAROUND Due to bug detailed in DatePickerFragment we do not use the onDateSet listener
         // keeping the code around for the future though as it is the preferred way to handle selections
         // datePickerFragment.setCallback(this);
         datePickerFragment.setAcceptDateListener(this);
         datePickerFragment.show(getFragmentManager(), null);
     }
-    
+
     // WORKAROUND this listener is not currently being used because of system bug detailed in DatePickerFragment
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day)
@@ -247,7 +286,7 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
         receipt.setTimestamp(date.getTime());
         timeView.setText(receipt.getDateAndTime(applicationContext));
     }
-    
+
     @Override
     public void onClick(DialogInterface dialog, int which)
     {
@@ -257,43 +296,43 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
         Date date = calendar.getTime();
         receipt.setTimestamp(date.getTime());
         timeView.setText(receipt.getDateAndTime(applicationContext));
-        
+
     }
 
     public Receipt saveReceipt()
     {
-        
-        if(setViewValues())
+
+        if (setViewValues())
         {
-            if(communicator.saveReceipt(receipt))
+            if (communicator.saveReceipt(receipt))
             {
                 Intent intent = new Intent(getActivity(), MainActivity.class);
                 intent.putExtra(Receipt.EXTRA_RECEIPT, receipt);
                 getActivity().startActivity(intent);
             }
-            
+
             return receipt;
         }
         else
         {
             return null;
         }
-        
+
     }
-    
+
     private boolean setViewValues()
     {
         // FIXME FC when map has no location! Get last known location not working?
         int latitude = MyMapActivity.currentGeoPoint.getLatitudeE6();
         int longitude = MyMapActivity.currentGeoPoint.getLongitudeE6();
-        
-        String name = nameView.getText().toString(); 
-        if(name.equals(""))
+
+        String name = nameView.getText().toString();
+        if (name.equals(""))
         {
             communicator.showToast("You need to fill in a name of your receipt.");
             return false;
         }
-        if(receipt.getPhoto().equals(""))
+        if (receipt.getPhoto().equals(""))
         {
             communicator.showToast("You need add a photo of your receipt.");
             return false;
@@ -305,7 +344,7 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
         receipt.setTax(taxView.getText().toString());
         receipt.setComment(commentView.getText().toString());
         receipt.setReceiptAccountId(receiptAccounts.get(accountSpinner.getSelectedItemPosition()).getCode());
-        
+
         return true;
     }
 
@@ -313,9 +352,8 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
     {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         filename = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".jpg";
-        File mediaStorageDir = new File(
-                                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                                    getActivity().getString(R.string.tag_receipttracker));
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), getActivity()
+                .getString(R.string.tag_receipttracker));
 
         if (!mediaStorageDir.exists())
         {
@@ -324,7 +362,7 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
                 Log.d(getActivity().getString(R.string.tag_receipttracker), "failed to create directory");
             }
         }
-        
+
         File photo = new File(mediaStorageDir.getPath(), filename);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
         receipt.setPhoto(photo.getAbsolutePath());
@@ -345,13 +383,21 @@ public class ReceiptAddFragment extends Fragment implements OnDateSetListener, D
                 }
         }
     }
-    
+
     private void showBitmap()
     {
         ScaleBitmapFileTask worker = new ScaleBitmapFileTask(imageView, receipt.getPhoto());
         worker.execute(150, 150);
     }
 
+    @Override
+    public void onFinishEditDialog(int receiptAccountCode, String receiptAccountName)
+    {
+        ReceiptAccount receiptAccount = new ReceiptAccount(-1, receiptAccountCode, receiptAccountName);
+        receipt.setReceiptAccountId(receiptAccountCode);
+        communicator.saveReceiptAccount(receiptAccount);
+        receiptAccounts.add(receiptAccount);
+        setReceiptAccountAdapter();
+    }
 
-    
 }
