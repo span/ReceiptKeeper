df00b5fd3a83ab64253f4fbf6d3e7b565d49534a
Daniel Kvist <daniel@danielkvist.net>
2012-08-21 14:42:20 +0200

    Refactored some try/catch stuff

--- src/net/danielkvist/util/Communicator.java
+++ src/net/danielkvist/util/Communicator.java
@@ -27,47 +27,38 @@ public class Communicator
     public Communicator(Context context)
     {
         this.context = context;
-        dbAdapter = new DbAdapter(context);
+        this.dbAdapter = new DbAdapter(context);
     }
-    
+
     public boolean deleteReceipt(Receipt receipt)
     {
         String pathToFile = receipt.getPhoto();
         File file = new File(pathToFile);
         boolean deleted = file.delete();
         boolean result = false;
-        try
+
+        if(openDatabase())
         {
-            dbAdapter.open();
             dbAdapter.deleteReceipt(receipt.getId());
             showToast(MESSAGE_RECEIPT_WAS_DELETED);
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
-        if(!deleted)
+        if (!deleted)
         {
             Log.d(context.getString(R.string.tag_receipttracker), "Failed to delete file: " + pathToFile);
             showToast(MESSAGE_COULD_NOT_DELETE_FILE);
         }
         return result && deleted;
     }
-    
+
     public Receipt getLatestReceipt()
     {
         Receipt receipt = null;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             Cursor cursor = dbAdapter.fetchLastReceipt();
             receipt = buildReceipt(cursor);
-            dbAdapter.close();
-        }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
+            closeDatabase();
         }
         return receipt;
     }
@@ -75,54 +66,39 @@ public class Communicator
     public ArrayList<Receipt> getReceipts(int limit)
     {
         ArrayList<Receipt> receiptList = null;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             Cursor cursor = dbAdapter.fetchReceipts(limit);
             receiptList = buildReceiptList(cursor);
-            dbAdapter.close();
-        }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
+            closeDatabase();
         }
         return receiptList;
     }
-    
+
     public ArrayList<Receipt> getReceipts(long timeFrom, long timeTo)
     {
         ArrayList<Receipt> receiptList = null;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             Cursor cursor = dbAdapter.fetchReceipts(timeFrom, timeTo);
             receiptList = buildReceiptList(cursor);
-            dbAdapter.close();
-        }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
+            closeDatabase();
         }
         return receiptList;
     }
-    
+
     public ArrayList<Receipt> searchReceipts(String query)
     {
         ArrayList<Receipt> receiptList = null;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             Cursor cursor = dbAdapter.searchReceiptName(query);
             receiptList = buildReceiptList(cursor);
-            dbAdapter.close();
-        }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
+            closeDatabase();
         }
         return receiptList;
     }
-    
+
     public boolean saveReceipt(Receipt receipt)
     {
         if (receipt.getId() > 0)
@@ -138,89 +114,65 @@ public class Communicator
     private boolean insertReceipt(Receipt receipt)
     {
         boolean result = false;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             result = dbAdapter.createReceipt(receipt.getName(), receipt.getPhoto(), receipt.getTimestamp(), receipt.getLocationLat(),
                     receipt.getLocationLong(), receipt.getSum(), receipt.getTax(), receipt.getComment(), receipt.getReceiptAccountId());
-            showResult(result);
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
+        showResult(result);
         return result;
     }
-    
+
     public boolean updateReceipt(Receipt receipt)
     {
         boolean result = false;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             result = dbAdapter.updateReceipt(receipt.getId(), receipt.getName(), receipt.getPhoto(), receipt.getTimestamp(),
                     receipt.getLocationLat(), receipt.getLocationLong(), receipt.getSum(), receipt.getTax(), receipt.getComment(),
                     receipt.getReceiptAccountId());
-            showResult(result);
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
+        showResult(result);
         return result;
     }
-    
+
     public boolean deleteReceiptAccount(ReceiptAccount receiptAccount)
     {
         boolean result = false;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             result = dbAdapter.deleteReceiptAccount(receiptAccount.getCode());
-            showResult(result);
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
+        showResult(result);
         return result;
     }
 
     public ArrayList<ReceiptAccount> getReceiptAccounts()
     {
         ArrayList<ReceiptAccount> receiptAccountList = null;
-
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             Cursor cursor = dbAdapter.fetchReceiptAccounts();
-
             if (cursor != null)
             {
                 ReceiptAccount receiptAccount;
                 receiptAccountList = new ArrayList<ReceiptAccount>();
                 while (!cursor.isAfterLast())
                 {
-                    receiptAccount = new ReceiptAccount(cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_ROWID)),
-                                                        cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_CODE)),
-                                                        cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_NAME)));
+                    receiptAccount = new ReceiptAccount(cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_ROWID)), cursor.getInt(cursor
+                            .getColumnIndex(DbAdapter.KEY_CODE)), cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_NAME)));
                     receiptAccountList.add(receiptAccount);
                     cursor.moveToNext();
                 }
             }
-
-            dbAdapter.close();
-        }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
+            closeDatabase();
         }
         return receiptAccountList;
     }
-    
+
     public boolean saveReceiptAccount(ReceiptAccount receiptAccount)
     {
         if (receiptAccount.getRowId() > 0)
@@ -232,47 +184,36 @@ public class Communicator
             return insertReceiptAccount(receiptAccount);
         }
     }
-    
+
     private boolean insertReceiptAccount(ReceiptAccount receiptAccount)
     {
         boolean result = false;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             result = dbAdapter.createReceiptAccount(receiptAccount.getCode(), receiptAccount.getName());
-            showResult(result);
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
+        showResult(result);
         return result;
     }
 
     private boolean updateReceiptAccount(ReceiptAccount receiptAccount)
     {
         boolean result = false;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             result = dbAdapter.updateReceiptAccount(receiptAccount.getRowId(), receiptAccount.getCode(), receiptAccount.getName());
-            showResult(result);
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
+        showResult(result);
         return result;
     }
 
     public HashMap<String, Integer> getAllSettings()
     {
         HashMap<String, Integer> settingsMap = null;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             Cursor cursor = dbAdapter.fetchAllSettings();
             settingsMap = new HashMap<String, Integer>();
             if (cursor != null)
@@ -284,33 +225,23 @@ public class Communicator
                     cursor.moveToNext();
                 }
             }
-
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
+        
         return settingsMap;
     }
 
     public int getSettingValue(String name)
     {
         int value = -1;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             Cursor cursor = dbAdapter.fetchSetting(name);
             if (cursor != null)
             {
                 value = cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_SETTING_VALUE));
             }
-
-            dbAdapter.close();
-        }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
+            closeDatabase();
         }
         return value;
     }
@@ -323,24 +254,19 @@ public class Communicator
     private boolean updateSetting(Setting setting)
     {
         boolean result = false;
-        try
+        if(openDatabase())
         {
-            dbAdapter.open();
             result = dbAdapter.updateSetting(setting.getName(), setting.getValue());
-            showResult(result);
-            dbAdapter.close();
+            closeDatabase();
         }
-        catch (SQLException e)
-        {
-            catchSQLException(e);
-        }
+        showResult(result);
         return result;
     }
 
     private ArrayList<Receipt> buildReceiptList(Cursor cursor)
     {
         ArrayList<Receipt> receiptList = null;
-        if(cursor != null)
+        if (cursor != null)
         {
             Receipt receipt;
             receiptList = new ArrayList<Receipt>();
@@ -349,7 +275,7 @@ public class Communicator
                 receipt = buildReceipt(cursor);
                 receiptList.add(receipt);
                 cursor.moveToNext();
-            } 
+            }
         }
         return receiptList;
     }
@@ -360,20 +286,39 @@ public class Communicator
         if (cursor.getCount() > 0)
         {
             receipt = new Receipt(cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_ROWID)), cursor.getString(cursor
-                .getColumnIndex(DbAdapter.KEY_NAME)), cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_PHOTO)), cursor.getLong(cursor
-                .getColumnIndex(DbAdapter.KEY_TIMESTAMP)), cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_LOCATION_LAT)),
-                cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_LOCATION_LONG)), cursor.getString(cursor
-                        .getColumnIndex(DbAdapter.KEY_SUM)), cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_TAX)),
-                cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_COMMENT)), cursor.getInt(cursor
-                        .getColumnIndex(DbAdapter.KEY_ACCOUNT_ID)));
+                    .getColumnIndex(DbAdapter.KEY_NAME)), cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_PHOTO)),
+                    cursor.getLong(cursor.getColumnIndex(DbAdapter.KEY_TIMESTAMP)), cursor.getString(cursor
+                            .getColumnIndex(DbAdapter.KEY_LOCATION_LAT)), cursor.getString(cursor
+                            .getColumnIndex(DbAdapter.KEY_LOCATION_LONG)), cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_SUM)),
+                    cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_TAX)), cursor.getString(cursor
+                            .getColumnIndex(DbAdapter.KEY_COMMENT)), cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_ACCOUNT_ID)));
         }
         return receipt;
     }
-    
+
     private void catchSQLException(SQLException e)
     {
         Log.d(context.getString(R.string.tag_receipttracker), e.getMessage());
         showToast(MESSAGE_COULD_NOT_OPEN);
+    }
+    
+    private boolean openDatabase()
+    {
+        try
+        {
+            dbAdapter.open();
+            return true;
+        }
+        catch (SQLException e)
+        {
+            catchSQLException(e);
+            return false;
+        }
+    }
+
+    private void closeDatabase()
+    {
+        dbAdapter.close();
     }
 
     private void showResult(boolean result)
