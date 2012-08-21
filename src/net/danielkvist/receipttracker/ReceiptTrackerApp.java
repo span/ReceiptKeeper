7f4be067f2a5ba656d33b5c6a1423206f7be01ae
Daniel Kvist <daniel@danielkvist.net>
2012-08-21 10:08:10 +0200

    Documented Activities

--- src/net/danielkvist/receipttracker/ReceiptTrackerApp.java
+++ src/net/danielkvist/receipttracker/ReceiptTrackerApp.java
@@ -3,7 +3,13 @@ package net.danielkvist.receipttracker;
 import net.danielkvist.util.BitmapLoader;
 import android.app.Application;
 
-
+/**
+ * This is the main Application wrapper for the app. This class handles
+ * some Singleton instances that need to be globally available such as the
+ * BitmapLoader and it's cache.
+ * @author Daniel Kvist
+ *
+ */
 public class ReceiptTrackerApp extends Application
 {
     public BitmapLoader bitmapLoader;
