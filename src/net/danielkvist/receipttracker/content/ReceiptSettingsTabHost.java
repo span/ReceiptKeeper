d13a10e2d9051649560439e648ea303ec3131c04
Daniel Kvist <daniel@danielkvist.net>
2012-08-24 16:24:57 +0200

    Refactored out the views from settings fragment into the tabhost

--- src/net/danielkvist/receipttracker/content/ReceiptSettingsTabHost.java
+++ src/net/danielkvist/receipttracker/content/ReceiptSettingsTabHost.java
@@ -1,24 +1,47 @@
 package net.danielkvist.receipttracker.content;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import net.danielkvist.receipttracker.R;
+import net.danielkvist.receipttracker.adapter.ReceiptAccountAdapter;
 import net.danielkvist.receipttracker.listener.AnimatedTabHostListener;
+import net.danielkvist.receipttracker.listener.EditTextCodeListener;
+import net.danielkvist.util.Communicator;
+import net.danielkvist.util.Setting;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 /**
  * This is a custom TabHost that uses an animated onTabChangeListener and also handles the showing/hiding of the menu
  * items in it's parent when switching between the tabs.
  * 
- * @author daniel
+ * @author Daniel Kvist
  * 
  */
-public class ReceiptSettingsTabHost extends TabHost
+public class ReceiptSettingsTabHost extends TabHost implements CompoundButton.OnCheckedChangeListener,
+RadioGroup.OnCheckedChangeListener, OnItemSelectedListener
 {
     private MenuItem deleteItem;
     private MenuItem saveItem;
     private MenuItem addItem;
+    private Context context;
+    private Spinner accountSpinner;
+    private ArrayList<ReceiptAccount> receiptAccounts;
+    private ReceiptAccountAdapter adapter;
+    private TextView accountName;
+    private TextView accountCode;
+    private Communicator communicator;
 
     /**
      * Only calls super for the parent constructor
@@ -28,6 +51,7 @@ public class ReceiptSettingsTabHost extends TabHost
     public ReceiptSettingsTabHost(Context context)
     {
         super(context);
+        this.context = context;
     }
 
     /**
@@ -39,6 +63,7 @@ public class ReceiptSettingsTabHost extends TabHost
     public ReceiptSettingsTabHost(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        this.context = context;
     }
 
     /**
@@ -101,6 +126,207 @@ public class ReceiptSettingsTabHost extends TabHost
                 }
             }
         });
+        
+        setupViews();
+    }
+    
+    /**
+     * Gets the current code in the code field
+     * @return the code
+     */
+    public long getCurrentCode()
+    {
+        return Long.parseLong(accountCode.getText().toString());
+    }
+    
+    /**
+     * Gets the current name in the name field
+     * @return the name
+     */
+    public String getCurrentName()
+    {
+        return accountName.getText().toString();
+    }
+    
+    /**
+     * Gets the receipt accounts list
+     * @return the list with all the accounts
+     */
+    public ArrayList<ReceiptAccount> getReceiptAccounts()
+    {
+        return receiptAccounts;
+    }
+    
+    /**
+     * Gets the currently selected receipt account
+     * @return
+     */
+    public ReceiptAccount getSelectedReceiptAccount()
+    {
+        return receiptAccounts.get(accountSpinner.getSelectedItemPosition());
+    }
+    
+    /**
+     * Sets up the basic views and controls that the user can see and interact with on the screen.
+     */
+    private void setupViews()
+    {
+        communicator = new Communicator(context);
+        HashMap<String, Integer> settingsMap = communicator.getAllSettings();
+
+        ((RadioGroup) findViewById(R.id.radio_group_storage)).setOnCheckedChangeListener(this);
+
+        Switch sumSwitch = (Switch) findViewById(R.id.switch_sum);
+        sumSwitch.setChecked(settingsMap.get(Setting.SETTING_FIELD_SUM) == View.VISIBLE);
+        sumSwitch.setOnCheckedChangeListener(this);
+
+        Switch taxSwitch = (Switch) findViewById(R.id.switch_tax);
+        taxSwitch.setChecked(settingsMap.get(Setting.SETTING_FIELD_TAX) == View.VISIBLE);
+        taxSwitch.setOnCheckedChangeListener(this);
+
+        Switch commentSwitch = (Switch) findViewById(R.id.switch_comment);
+        commentSwitch.setChecked(settingsMap.get(Setting.SETTING_FIELD_COMMENT) == View.VISIBLE);
+        commentSwitch.setOnCheckedChangeListener(this);
+
+        Switch accountSwitch = (Switch) findViewById(R.id.switch_account);
+        accountSwitch.setChecked(settingsMap.get(Setting.SETTING_FIELD_ACCOUNT) == View.VISIBLE);
+        accountSwitch.setOnCheckedChangeListener(this);
+
+        Switch locationSwitch = (Switch) findViewById(R.id.switch_location);
+        locationSwitch.setChecked(settingsMap.get(Setting.SETTING_FIELD_LOCATION) == View.VISIBLE);
+        locationSwitch.setOnCheckedChangeListener(this);
+
+        accountSpinner = (Spinner) findViewById(R.id.account_spinner);
+        accountSpinner.setOnItemSelectedListener(this);
+        receiptAccounts = communicator.getReceiptAccounts();
+
+        adapter = new ReceiptAccountAdapter(context, android.R.layout.simple_spinner_item, receiptAccounts);
+        accountSpinner.setAdapter(adapter);
+
+        accountName = (TextView) findViewById(R.id.account_name);
+        accountCode = (TextView) findViewById(R.id.account_code);
+        accountCode.setOnKeyListener(new EditTextCodeListener(communicator));
+        
+    }
+
+    /**
+     * Listener for the RadioGroup which contains the radio buttons that saves the current setting to the database.
+     */
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId)
+    {
+        // XXX Add cloud storage Setting and make it do something :P
+        Setting setting = new Setting();
+        setting.setName("storage");
+        switch (checkedId)
+        {
+            case R.id.storage_local:
+                setting.setValue(Setting.SETTING_STORAGE_LOCAL);
+                break;
+            case R.id.storage_cloud:
+                setting.setValue(Setting.SETTING_STORAGE_CLOUD);
+                break;
+        }
+        communicator.saveSetting(setting);
+    }
+
+    /**
+     * Listener for the switches that saves each Setting as needed when they change.
+     */
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+    {
+        Setting setting = new Setting();
+        switch (buttonView.getId())
+        {
+            case R.id.switch_location:
+                setting.setName(Setting.SETTING_FIELD_LOCATION);
+                break;
+            case R.id.switch_sum:
+                setting.setName(Setting.SETTING_FIELD_SUM);
+                break;
+            case R.id.switch_tax:
+                setting.setName(Setting.SETTING_FIELD_TAX);
+                break;
+            case R.id.switch_comment:
+                setting.setName(Setting.SETTING_FIELD_COMMENT);
+                break;
+            case R.id.switch_account:
+                setting.setName(Setting.SETTING_FIELD_ACCOUNT);
+                break;
+        }
+        setting.setValue(isChecked ? View.VISIBLE : View.GONE);
+
+        communicator.saveSetting(setting);
+    }
+
+    /**
+     * When an item in the Spinner has been selected we want to update the fields with the correct information.
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3)
+    {
+        updateFields();
+    }
+
+    /**
+     * Helper method that gets the current selected position of the Spinner and sets the edit text views accordingly. If
+     * the code is not user provided we disable the fields for editing.
+     */
+    public void updateFields()
+    {
+        String displayName;
+        int position = accountSpinner.getSelectedItemPosition();
+        if (position >= receiptAccounts.size())
+        {
+            // If we remove the last item in the list, it will be out of bounds when we update the fields
+            // so we have to decrement with one to get the last item.
+            position--;
+        }
+        ReceiptAccount receiptAccount = receiptAccounts.get(position);
+        String name = receiptAccount.getName();
+        long code = receiptAccount.getCode();
+        if (!receiptAccount.isUserAdded())
+        {
+            accountName.setEnabled(false);
+            accountCode.setEnabled(false);
+            displayName = getResources().getString(getResources().getIdentifier(name, "string", "net.danielkvist.receipttracker"));
+        }
+        else
+        {
+            accountName.setEnabled(true);
+            accountCode.setEnabled(true);
+            displayName = name;
+        }
+        accountName.setText(displayName);
+        accountCode.setText(String.valueOf(code));
+    }
+
+    /**
+     * Not used.
+     */
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0)
+    {
+        /* Nothing selected in spinner so we don't have to do anything, yawn */
+    }
+
+    /**
+     * Sets the selected spinner item with the corresponding code
+     * @param code the receipt code
+     */
+    public void setSelectedSpinnerItem(long code)
+    {
+        accountSpinner.setSelection(adapter.findReceiptPosition(code));
+    }
+
+    /**
+     * Notifies the Spinner adapter that the data has changed.
+     */
+    public void notifyDataSetChanged()
+    {
+        adapter.notifyDataSetChanged();
+        
     }
 
 }
