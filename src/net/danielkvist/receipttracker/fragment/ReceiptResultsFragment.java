685121b6e3de1d3c887a378c6806c95d2fd34c32
Daniel Kvist <daniel@danielkvist.net>
2012-08-26 17:32:20 +0200

    No idea

--- src/net/danielkvist/receipttracker/fragment/ReceiptResultsFragment.java
+++ src/net/danielkvist/receipttracker/fragment/ReceiptResultsFragment.java
@@ -1,15 +1,44 @@
 package net.danielkvist.receipttracker.fragment;
 
+import net.danielkvist.receipttracker.R;
+import net.danielkvist.util.Communicator;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ReceiptResultsFragment extends Fragment
 {
+    private Communicator communicator;
 
-    public static final int ID = 3;
-
+    /**
+     * Just an empty constructor
+     */
     public ReceiptResultsFragment()
     {
-        // TODO Auto-generated constructor stub
+
     }
 
+    /**
+     * Sets retain instance to true
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        communicator = new Communicator(getActivity());
+    }
+    
+    /**
+     * Calls two helper methods that setup the tabs and the setting controls
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_receipt_results, container, false);
+
+        return rootView;
+    }
 }
