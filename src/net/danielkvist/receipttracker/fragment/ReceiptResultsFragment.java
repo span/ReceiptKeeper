20ad17e1bf8634a2625540a8717c603f656a2618
Daniel Kvist <daniel@danielkvist.net>
2012-08-26 17:05:31 +0200

    Added results layout and base class

---
+++ src/net/danielkvist/receipttracker/fragment/ReceiptResultsFragment.java
@@ -0,0 +1,15 @@
+package net.danielkvist.receipttracker.fragment;
+
+import android.app.Fragment;
+
+public class ReceiptResultsFragment extends Fragment
+{
+
+    public static final int ID = 3;
+
+    public ReceiptResultsFragment()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
